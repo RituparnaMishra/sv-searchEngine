72470eade0eef03bbe3b3cadef991ea1d793b5ff
Rituparna Mishra <mishra@medialesson.de>
2022-02-12 20:16:36 +0100

    Data models and data srvice set up

---
+++ sv-searchEngine/sv-searchEngine/Models/EntityModel.cs
@@ -0,0 +1,17 @@
+﻿using sv_searchEngine.Models.DTOS;
+
+
+namespace sv_searchEngine.Models
+{
+    public class EntityModel
+    {
+        //public DataCollection Entity { get; set; }
+
+        public IEnumerable<Building> Buildings { get; set; } = new List<Building>();
+        public IEnumerable<Lock> Locks { get; set; } = new List<Lock>();
+        public IEnumerable<Group> Groups { get; set; } = new List<Group>();
+        public IEnumerable<Media> Media { get; set; } = new List<Media>();
+
+
+    }
+}
---
+++ sv-searchEngine/sv-searchEngine/Services/DataService.cs
@@ -0,0 +1,58 @@
+﻿
+using Newtonsoft.Json;
+using sv_searchEngine.Models;
+using System.Net;
+
+namespace sv_searchEngine.Services
+{
+    public interface IDataService
+    {
+        Task<EntityModel?> GetDataWithSearchCriteria(string searchCriteria);
+    }
+    public class DataService : IDataService
+    {
+        private const string DataFileLink = "https://simonsvoss-homework.herokuapp.com/sv_lsm_data.json";
+        EntityModel _data = new();
+
+        private async Task ReadDataFromJsonFile()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var result = await client.GetAsync(DataFileLink);
+                    if (result != null && result.StatusCode == HttpStatusCode.OK && result.Content != null)
+                    {
+
+                        var stringData = await result.Content.ReadAsStringAsync();
+                        if (!string.IsNullOrEmpty(stringData))
+                        {
+                            _data = JsonConvert.DeserializeObject<EntityModel>(stringData);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Cannot read the data file due to:{e.InnerException}");
+            }
+        }
+
+        public async Task<EntityModel?> GetDataWithSearchCriteria(string searchCriteria)
+        {
+            await ReadDataFromJsonFile();
+            if (!string.IsNullOrEmpty(searchCriteria) && _data != null)
+            {
+                _data.Buildings = _data.Buildings.Where(b => b.Name.Contains(searchCriteria, StringComparison.OrdinalIgnoreCase)).ToList();
+                if (_data.Buildings.Any())
+                {
+                    _data.Locks = _data.Locks.Where(l => _data.Buildings.Select(b => b.Id).Contains(l.BuildingId)).ToList();
+                }
+            }
+
+
+            return _data;
+        }
+
+    }
+}
